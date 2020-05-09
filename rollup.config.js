86187701768b26e3525301530d2ed83257bbb1a9
kahverengi <ahmet.duruer@yandex.com>
2020-05-09 18:41:54 +0300

    Prepare development environment

---
+++ rollup.config.js
@@ -0,0 +1,84 @@
+import svelte from "rollup-plugin-svelte";
+import resolve from "@rollup/plugin-node-resolve";
+import replace from "@rollup/plugin-replace";
+import livereload from "rollup-plugin-livereload";
+import {terser} from "rollup-plugin-terser";
+import rmdir from "rimraf";
+
+rmdir("public/assets", function (error) {});
+
+const production = !process.env.ROLLUP_WATCH;
+
+const input = ["test/main.js"];
+
+const watch = {
+  clearScreen: false,
+};
+
+const plugins = [
+  svelte({
+    // enable run-time checks when not in production
+    dev: !production,
+  }),
+
+  // If you have external dependencies installed from
+  // npm, you'll most likely need these plugins. In
+  // some cases you'll need additional configuration -
+  // consult the documentation for details:
+  // https://github.com/rollup/plugins/tree/master/packages/commonjs
+  resolve({
+    browser: true,
+    dedupe: ["svelte"],
+  }),
+
+  replace({
+    "process.env.NODE_ENV": JSON.stringify(
+      production ? "production" : "development"
+    ),
+  }),
+
+  // In dev mode, call `npm run start` once
+  // the bundle has been generated
+  !production && serve(),
+
+  // Watch the `public` directory and refresh the
+  // browser on changes when not in production
+  !production && livereload("public"),
+
+  // If we're building for production (npm run build
+  // instead of npm run dev), minify
+  production && terser(),
+];
+
+const esExport = {
+  input: input,
+  output: [
+    {
+      sourcemap: true,
+      format: "es",
+      name: "app",
+      dir: "public/assets/js/es/",
+    },
+  ],
+  plugins: plugins,
+  watch: watch,
+};
+
+export default [esExport];
+
+function serve() {
+  let started = false;
+
+  return {
+    writeBundle() {
+      if (!started) {
+        started = true;
+
+        require("child_process").spawn("npm", ["run", "start", "--", "--dev"], {
+          stdio: ["ignore", "inherit", "inherit"],
+          shell: true,
+        });
+      }
+    },
+  };
+}
