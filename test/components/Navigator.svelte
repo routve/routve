9f6f33defd9976f1e9887551dd544df421c2f9fa
kahverengi <ahmet.duruer@yandex.com>
2020-05-10 17:14:12 +0300

    Change name of a link in navigator in test

--- test/components/Navigator.svelte
+++ test/components/Navigator.svelte
@@ -5,7 +5,7 @@
 <a href="/test-page-async">Test Page Dynamic Load</a> <br>
 <a href="/nested-route-page">Test Nested Route Page</a> <br>
 <a href="/nested-route-page/example">Example Nested Route Page</a> <br>
-<a href="/nested-route-params-page">Example Nested Route With Params Page</a> <br>
+<a href="/nested-route-params-page">Example Nested Route With Params Default Page</a> <br>
 <a href="/nested-route-params-page/example/10">Example Nested Route With Params Page</a> <br>
 <a href="/nested-route-params-page/53">Example Nested Route With Page Params Test</a> <br>
 <a href="/error404">Test Error Page</a>
