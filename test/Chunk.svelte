748a7bcb0ee7268b459a0de42ffe41554229638d
Berk Altıok <berkpw@gmail.com>
2020-05-10 12:15:37 +0300

    Create example localChunk

---
+++ test/Chunk.svelte
@@ -0,0 +1,18 @@
+<script>
+  import Loadable from "svelte-loadable";
+
+  export let component;
+  export let dynamicImport;
+  export let delay = 0;
+  export let params = {};
+</script>
+
+<!--suppress JSUnusedAssignment, BadExpressionStatementJS, ES6UnusedImports -->
+<Loadable loader="{dynamicImport}" delay="delay">
+  <div slot="loading"></div>
+
+  <div slot="success" let:component>
+    <svelte:component this="{component}" {...params} />
+    Test
+  </div>
+</Loadable>
