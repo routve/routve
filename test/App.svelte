72fdd01c6f3f1bbe4b954718c0b1869f7468dbb2
kahverengi <ahmet.duruer@yandex.com>
2020-05-12 22:53:08 +0300

    Add TODO for beforeRouteEnter and afterRouteEnter events

--- test/App.svelte
+++ test/App.svelte
@@ -3,6 +3,19 @@
   import RouterConfig from "./router.config";
 
   import Navigator from "./components/Navigator.svelte";
+
+  //TODO @Berk
+  // beforeRouteEnter((context, next) => {
+  //   console.log("BeforeRouteEnter" + context.pathname)
+  //
+  //   next();
+  // })
+  //
+  // afterRouteEnter((context, next) => {
+  //   console.log("afterRouteEnter" + context.pathname)
+  //
+  //   next();
+  // })
 </script>
 
 <h1>Welcome to Routve!</h1>
