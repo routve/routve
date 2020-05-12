1624b986db2cff5a6b69eed1760a5042a799280d
kahverengi <ahmet.duruer@yandex.com>
2020-05-12 15:14:48 +0300

    Disabled a11y warning on navigator in test

--- test/components/Navigator.svelte
+++ test/components/Navigator.svelte
@@ -12,5 +12,6 @@
 <a href="/nested-route-params-page">Example Nested Route With Params Default Page</a> <br>
 <a href="/nested-route-params-page/example/10">Example Nested Route With Params Page</a> <br>
 <a href="/nested-route-params-page/53">Example Nested Route With Page Params Test</a> <br>
+<!-- svelte-ignore a11y-invalid-attribute -->
 <a href="javascript:void(0);" on:click="{() => route('/programmatically-routed')}">The Page Programmatically Routed</a> <br>
 <a href="/error404">Test Error Page</a>
