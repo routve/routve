9b11d560c0f1709613364e5ed014280b27267a6b
Berk Altıok <berkpw@gmail.com>
2020-06-02 20:01:11 +0300

    Change chunk class to function (Fix: minify name problem)

--- src/ChunkGenerator.js
+++ src/ChunkGenerator.js
@@ -1,17 +1,26 @@
 // original source: https://github.com/hmmhmmhm/svelte-spa-chunk/blob/master/src/index.ts
 import ChunkLoadable from "./Chunk.svelte";
 
+const isClass = (func) => {
+  return typeof func === 'function'
+    && /^class\s/.test(Function.prototype.toString.call(func));
+}
+
 const ChunkGenerator = (dynamicImport, chunk = ChunkLoadable) => {
   return Chunk(dynamicImport, chunk);
 };
 
 export const Chunk = (dynamicImport, Component) => {
-  return class SvelteComponentHook {
-    constructor(options) {
-      options.props = {
-        ...options.props,
-        dynamicImport
-      };
+  return function(options) {
+    options.props = {
+      ...options.props,
+      dynamicImport
+    };
+
+    if (isClass(dynamicImport)) {
+      options.props = {};
+      return new dynamicImport(options);
+    } else {
       return new Component(options);
     }
   };
