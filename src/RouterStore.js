cde57735007ffe60b35b52a20925a890158d88b7
kahverengi <ahmet.duruer@yandex.com>
2020-12-19 15:13:57 +0300

    fix(hashbang): fix routing when basePath given as "" and hashbang enabled

--- src/RouterStore.js
+++ src/RouterStore.js
@@ -5,5 +5,5 @@ export const isPageLoading = writable(false);
 export const isComponentLoading = writable(true);
 export const isRouteLoading = writable(true);
 
-export const path = writable("");
+export const path = writable(null);
 export const basePageInstance = page.create();
